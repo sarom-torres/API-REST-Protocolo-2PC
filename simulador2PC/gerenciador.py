12ed5406af34c10b6dd61302dd58ef166cc23fd6
saromtorres <saromtorres@gmail.com>
2019-11-23 22:32:48 -0200

    subindo arquivo replicas

--- simulador2PC/gerenciador.py
+++ simulador2PC/gerenciador.py
@@ -1,7 +1,8 @@
 import json
 
+
+# é para ler de um arquivo já dentro do projeto ou deve ser passado
 def leitura_arq():
     arquivo = open('dados.txt', 'r')
     dados = arquivo.read()
-
     return json.loads(dados)
--- simulador2PC/replicador.py
+++ simulador2PC/replicador.py
@@ -1,8 +1,26 @@
 import sys
 import flask
 import flask_httpauth
+from flask import Flask, jsonify, request, abort
+
 from gerenciador import leitura_arq
 
+
+log = Flask(__name__)
+
+dados_json = leitura_arq()
+
+
+@log.route('/contas',methods=['GET'])
+def obter_contas():
+    return dados_json
+
+@log.route('/replicas',methods=['POST'])
+def carregar_replicas():
+    if not request.json:
+        abort(400)
+    replicas = request.json
+    return jsonify('replicas',replicas),201
 
 if __name__ == "__main__":
 
@@ -13,7 +31,7 @@ if __name__ == "__main__":
         tipo = 'replica'
 
     print(tipo, "online...")
-
-    dados_json = leitura_arq()
-
     print(dados_json)
+
+    log.run(host='0.0.0.0',debug=True)
+
